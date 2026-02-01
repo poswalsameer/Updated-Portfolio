d2a5cac6720fd65ebf941296d5c9dba04c33709a
poswalsameer <poswalsameer2003@gmail.com>
2026-02-01 18:55:11 +0530

    added better style on text selection

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/footer";
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-screen pt-3 sm:pt-10 justify-between">
+    <main className="selection:bg-zinc-300 selection:text-zinc-900 flex flex-col min-h-screen pt-3 sm:pt-10 justify-between">
       <div>
         <Header />
         <PortfolioBody />
